32f51ff605014ce4e1b55a422e01d5ec5718d1e0
Rocky971 <rkrocky2018@gmail.com>
2023-01-21 22:03:35 +0600

    added

--- src/Components/PoorFundActiveMember/PoorFundActiveMember.js
+++ src/Components/PoorFundActiveMember/PoorFundActiveMember.js
@@ -6,7 +6,7 @@ const PoorFundActiveMember = () => {
             <div className="p-14 text-center flex justify-center bg-[#02a95c] border custom border-r">
                 <i class="fa-solid fa-handshake-angle text-6xl p-[18px] custom-transform"></i>
                 <div className="">
-                    <h2 className='text-5xl font-bold pb-[20px] custom-transform'>598K</h2>
+                    <h2 className='text-5xl font-bold pb-[20px] custom-transform'>597K</h2>
                     <p className='text-lg font-semibold custom-transform'>// POOR PEOPLE</p>
                 </div>
             </div>
